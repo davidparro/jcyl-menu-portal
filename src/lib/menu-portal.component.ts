18e1d9b6d3b8a777787a351193d64cc596cf4220
David Parro Rubio <davidparro@gmail.com>
2019-03-26 10:20:14 +0100

    Accesibilidad mejorada

--- src/lib/menu-portal.component.ts
+++ src/lib/menu-portal.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import { MenuItem } from './menu-portal-models';
 import { FocusMonitor } from '@angular/cdk/a11y';
+import { ENTER } from '@angular/cdk/keycodes';
 declare var $: any;
 
 @Component({
@@ -55,14 +56,33 @@ export class MenuPortalComponent implements OnInit, OnDestroy, AfterViewInit {
             const ele = document.getElementById('submenu' + index);
             if (ele) {
                 this.focusMonitor.monitor(ele, true)
-                    .subscribe(origin => this.ngZone.run(() => {
-                        if (origin) {
-                            ele.classList.add('activo');
-                        }
-                        this.cdr.markForCheck();
-                    }));
+                    .subscribe(
+                        origin => this.ngZone.run(
+                            () => {
+                                if (origin) {
+                                    if (origin === 'keyboard') {
+                                        $('#' + ele.getAttribute('id') + '>a.menu-focus').css('opacity', 1);
+                                    }
+                                    ele.classList.add('activo');
+                                    ele.classList.add('activo-focus');
+                                } else {
+                                    $('#' + ele.getAttribute('id') + '>a.menu-focus').css('opacity', 0);
+                                    ele.classList.remove('activo')
+                                    ele.classList.remove('activo-focus');
+                                }
+                                this.cdr.markForCheck();
+                            }
+                        )
+                    );
             }
         });
+    }
+
+    showSubmenu(event, element) {
+        if (ENTER === event.keyCode) {
+            document.getElementById(element).classList.remove('activo-focus');
+            document.getElementById(element).classList.add('activo');
+        }
     }
 
     ngOnDestroy() {
